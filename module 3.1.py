2a0c4f15f5e03d293444347a6ced67d93b42f5d0
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-25 00:23:24 +0300

    Add files via upload

---
+++ module 3.1.py
@@ -0,0 +1,22 @@
+calls = 0
+
+
+def count_calls():
+    globals(calls)
+    calls = 13
+    print()
+    print(calls)
+    print(count_calls)
+    string_info():
+    global
+    (calls)
+    calls = 15
+    print(calls)
+
+
+is_contains():
+globals(calls)
+calls = 16
+print()
+print(calls)
+print(is_contains)
